59978c298d05045111b7fca6dceebe8922e61e5a
muiruri7 <leon.w.muiruri@gmail.com>
2025-03-04 09:48:48 +0300

    done!

--- src/app/components/employee-detail/employee-detail.component.ts
+++ src/app/components/employee-detail/employee-detail.component.ts
@@ -1,28 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, RouterModule } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router'; 
 import { EmployeeService } from '../../services/employee.service';
 import { Employee } from '../../models/employee';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import { NgIf } from '@angular/common';
+import { NgIf, CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router'; 
 
 @Component({
   selector: 'app-employee-details',
   standalone: true,
   templateUrl: './employee-detail.component.html',
   styleUrls: ['./employee-detail.component.scss'],
-  imports: [MatCardModule, MatButtonModule, RouterModule, NgIf]
+  imports: [MatCardModule, MatButtonModule, RouterModule, NgIf, CommonModule] 
 })
 export class EmployeeDetailsComponent implements OnInit {
-deleteEmployee(arg0: number) {
-throw new Error('Method not implemented.');
-}
   employee?: Employee;
 
-  constructor(private route: ActivatedRoute, private employeeService: EmployeeService) {}
+  constructor(private route: ActivatedRoute, private router: Router, private employeeService: EmployeeService) {}
 
   ngOnInit() {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.employee = this.employeeService.getEmployeeById(id);
+  }
+
+  deleteEmployee(id: number) {
+    if (confirm("Are you sure you want to delete this employee?")) {
+      this.employeeService.deleteEmployee(id);
+      this.router.navigate(['/employees']);
+    }
   }
 }
